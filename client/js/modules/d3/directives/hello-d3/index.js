522327e1854452d42795ab23a980f37167572499
Norbert Pozsonyi <norbert.pozsonyi@gmail.com>
2016-07-27 19:56:10 +0200

    create 'hello-d3' directive to test d3 npm module

---
+++ client/js/modules/d3/directives/hello-d3/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+export default function helloD3Directive(d3) {
+  return {
+    restrict: 'E',
+    scope: {},
+    link: link
+  };
+
+  function link(scope, element) {
+    d3.select(element[0]).append('h2')
+      .text('Hello, D3!');
+  }
+}
+
+helloD3Directive.$inject = ['d3.js'];
--- client/js/modules/d3/index.js
+++ client/js/modules/d3/index.js
@@ -1,6 +1,9 @@
 import angular from 'angular';
 import * as d3 from 'd3';
 
+import helloD3Directive from './directives/hello-d3';
+
 export default angular.module('d3module', [])
   .factory('d3.js', [() => d3])
+  .directive('helloD3', helloD3Directive)
   .name;
