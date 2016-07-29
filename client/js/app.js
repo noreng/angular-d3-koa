65c1c622f996e29607de0f6013e41e195aa29f84
Norbert Pozsonyi <norbert.pozsonyi@gmail.com>
2016-07-29 11:34:46 +0200

    install and config 'ui-router'

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,19 @@
 'use strict';
 
 import angular from 'angular';
+import uiRouter from 'angular-ui-router';
 import d3module from './modules/d3';
 
-angular.module('d3App', [d3module]);
+angular.module('d3App', [d3module, uiRouter])
+  .config([ '$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/force-directed-graph');
+    $stateProvider
+      .state('force-directed-graph', {
+        url: '/force-directed-graph',
+        template: '<force-directed-graph graph-id="1"></force-directed-graph>'
+      })
+      .state('hello-d3', {
+        url: '/hellod3',
+        template: '<hello-d3></hello-d3>'
+      })
+  }]);
